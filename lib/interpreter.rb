b318b2d925ddf311bee2df7fb7fb57cef32faed3
leandronsp <leandronsp@gmail.com>
2023-09-21 21:19:50 -0300

    wip: trampoline all the way around

--- lib/interpreter.rb
+++ lib/interpreter.rb
@@ -7,28 +7,28 @@ class Interpreter
   def initialize
     @stack = []
     @executors = []
-    @scope = {}
   end
 
   def run(json_input)
     parsed = JSON.parse(json_input, symbolize_names: true)
     @stack.push(parsed[:expression])
+    scope = {}
 
     # Trampoline
     loop do 
       term = @stack.pop
       break if term.nil?
-      continuation, result = evaluate(term)
+      continuation, result, scope = evaluate(term, scope)
 
       case [continuation, result]
       in [:raw, result]
         executor = @executors.pop
-        continuation, result = executor&.call(result)
+        continuation, result, scope = executor&.call(result)
 
         next if continuation == :noop
 
         while executor = @executors.pop
-          continuation, result = executor&.call(result)
+          continuation, result, scope = executor&.call(result)
           break if continuation == :noop
         end
       in [:noop, nil]; next
@@ -37,15 +37,17 @@ class Interpreter
     end
   end
 
-  def evaluate(term)
+  def evaluate(term, scope = nil)
+    scope ||= {}
+
     case term
-    in { kind: 'Str', value: value }; [:raw, value.to_s]
-    in { kind: 'Int', value: value }; [:raw, value.to_i]
-    in { kind: 'Bool', value: value }; [:raw, value]
+    in { kind: 'Str', value: value }; [:raw, value.to_s, scope]
+    in { kind: 'Int', value: value }; [:raw, value.to_i, scope]
+    in { kind: 'Bool', value: value }; [:raw, value, scope]
     in { kind: 'Print', value: next_term }
       @executors.push(-> (result) { puts result })
       @stack.push(next_term)
-      [:noop, nil]
+      [:noop, nil, scope]
     in { kind: 'BinaryOp', op: op, lhs: lhs, rhs: rhs }
       @executors.push(-> (left) { 
         @executors.push(-> (left, right) { 
@@ -54,52 +56,70 @@ class Interpreter
                    in ['Add', _, _]; "#{left}#{right}"
                    in ['Sub', Integer, Integer]; left - right
                    in ['Eq', Integer, Integer]; left == right
+                   in ['Lt', Integer, Integer]; left < right
                    else raise "Unkown operation #{[op, left, right]}"
                    end
-          [:raw, result]
+          [:raw, result, scope]
         }.curry[left])
 
-        [:noop, nil]
+        [:noop, nil, scope]
       })
 
       @stack.push(rhs, lhs)
-      [:noop, nil]
+      [:noop, nil, scope]
     in { kind: 'Let', name: { text: text }, value: value, next: next_term }
       @executors.push(-> (result) { 
-        @scope[text] = result
-        [:noop, nil]
+        scope[text] = result
+        [:noop, nil, scope]
       })
 
       @stack.push(next_term, value)
-      [:noop, nil]
-    in { kind: 'Var', text: text }; [:raw, @scope[text]]
+      [:noop, nil, scope]
+    in { kind: 'Var', text: text }
+      [:raw, scope[text], scope]
     in { kind: 'If', condition: condition, then: then_term, otherwise: otherwise_term }
       @executors.push(-> (result) { 
         @stack.push(result ? then_term : otherwise_term)  
-        [:noop, nil]
+        [:noop, nil, scope]
       })
 
       @stack.push(condition)
-      [:noop, nil]
+      [:noop, nil, scope]
     in { kind: 'Function', parameters: parameters, value: value }
       @executors.pop.call(-> (*args) { 
         params = parameters.map { |param| param[:text] }
         fn_args = params.zip(args).to_h
-        @scope.merge!(fn_args)
+        scope.merge!(fn_args)
         @stack.push(value)
-        [:noop, nil]
+        [:noop, nil, scope]
       })
 
-      [:noop, nil]
+      [:noop, nil, scope]
     in { kind: 'Call', callee: callee, arguments: arguments }
-      args = arguments.map { |arg| evaluate(arg)[1] }
+      args = arguments.map do |arg|
+        case arg
+        in { kind: 'BinaryOp', op: op, lhs: lhs, rhs: rhs }
+          left = evaluate(lhs, scope)[1]
+          right = evaluate(rhs, scope)[1]
+
+          case [op, left, right]
+          in ['Add', Integer, Integer]; left + right
+          in ['Add', _, _]; "#{left}#{right}"
+          in ['Sub', Integer, Integer]; left - right
+          in ['Eq', Integer, Integer]; left == right
+          in ['Lt', Integer, Integer]; left < right
+          else raise "Unkown operation #{[op, left, right]}"
+          end
+        else evaluate(arg, scope)[1]
+        end
+      end
 
       @executors.push(-> (function) {
         function.call(*args)
       })
 
       @stack.push(callee)
-      [:noop, nil]
+      [:noop, nil, scope]
     else raise "Unkonwn term: #{term}"
     end
   end
